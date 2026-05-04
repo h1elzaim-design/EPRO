3bebd85e68842d300974be9e1ed5a2687ee58d53
h1elzaim-design <h1elzaim@gmail.com>
2026-05-04 22:02:45 +0200

    feat: implement merge sort algorithm in Aufgabe6.py

---
+++ Serie07/Aufgabe6.py
@@ -0,0 +1,23 @@
+def merge(left: list, right: list) -> list:
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+      
+def merge_sort(lst: list) -> list:
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    return merge(left, right) 
+
+print(merge_sort([1, 2, 3, 6, 9, 4, 5]))
