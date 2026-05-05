cbf67e349d584f067711030c8d894acfc97320ba
h1elzaim-design <h1elzaim@gmail.com>
2026-05-05 11:15:06 +0200

    feat: add merge sort helper and placeholder for assignment 6

---
+++ Serie07/Aufgabe5.py
@@ -0,0 +1,16 @@
+def merge(left: list, right: list) -> list: 
+    result = []
+    i,y = 0, 0
+    while i < len(left) and y < len(right): 
+        if left[i] <= right[y]: 
+            result.append(left[i])
+            i += 1
+        else: 
+            result.append(right[y])
+            y += 1
+    result.extend(left[i:])
+    result.extend(right[y:])
+    return result
+
+print(merge([1, 3, 5, 7, 10, 13], [2, 4, 6, 8, 9, 11, 12]))
+    
